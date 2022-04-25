34650f50f197ea72b443e99202f092ccbe074039
deepakporwal <er.deepakporwal@gmail.com>
2022-04-25 17:58:43 +0530

    simple counter example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { countReducer } from './counter.reducer';
+import { MycounterComponent } from './mycounter/mycounter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MycounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({count : countReducer}) 
   ],
   providers: [],
   bootstrap: [AppComponent]
